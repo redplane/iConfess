a236bd05d5ce4ae3d105832862398c6ab4152b9f
Nguyen Duy Linh <redplane_dt@yahoo.com.vn>
2016-12-03 16:49:52 +0700

    - Added view models of category.

---
+++ A-SOURCE_CODE/A-SERVICE/Shared/Models/Pagination.cs
@@ -0,0 +1,17 @@
+﻿namespace Shared.Models
+{
+    public class Pagination
+    {
+        /// <summary>
+        ///     Index of result page.
+        ///     Min: 0
+        ///     Max: (infinite)
+        /// </summary>
+        public int Index { get; set; }
+
+        /// <summary>
+        ///     Maximum records can be displayed per page.
+        /// </summary>
+        public int Record { get; set; }
+    }
+}
---
+++ A-SOURCE_CODE/A-SERVICE/Shared/ViewModels/Categories/FindCategoriesViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Shared.Models;
+
+namespace Shared.ViewModels.Categories
+{
+    public class FindCategoriesViewModel
+    {
+        /// <summary>
+        ///     Index of category.
+        /// </summary>
+        public int? Id { get; set; }
+
+        /// <summary>
+        ///     Index of creator.
+        /// </summary>
+        public int? Creator { get; set; }
+
+        /// <summary>
+        ///     Name of category.
+        /// </summary>
+        public TextSearch Name { get; set; }
+
+        /// <summary>
+        ///     When the category was created.
+        /// </summary>
+        public UnixDateRange Created { get; set; }
+
+        /// <summary>
+        ///     When the category was lastly modified.
+        /// </summary>
+        public UnixDateRange LastModified { get; set; }
+
+        /// <summary>
+        ///     Pagination information.
+        /// </summary>
+        public Pagination Pagination { get; set; }
+    }
+}
