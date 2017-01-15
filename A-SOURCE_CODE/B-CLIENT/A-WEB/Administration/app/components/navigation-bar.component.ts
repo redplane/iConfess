c912ed3e5b22df1391746886647c8f286254afb7
Akai <redplane_dt@yahoo.com.vn>
2017-01-15 15:49:54 +0700

    - Commit for change check

--- A-SOURCE_CODE/B-CLIENT/A-WEB/Administration/app/components/navigation-bar.component.ts
+++ A-SOURCE_CODE/B-CLIENT/A-WEB/Administration/app/components/navigation-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Sections} from "../enumerations/Sections";
+
 @Component({
     selector: 'navigation-bar',
     templateUrl: './app/views/navigation-bar.component.html'
